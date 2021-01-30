0e91a0a3871f45a9cf7dad6478a88802bd025ba3
BKlovak <klovak.bohdan@gmail.com>
2021-01-30 13:25:25 +0200

    v0.0.1

---
+++ js/index.js
@@ -0,0 +1,33 @@
+let playList = [
+    {
+        name: 'Never Gonna Give You Up',
+        artist: 'Rick Astley',
+        src: 'content/RickAstley-NeverGonnaGiveYouUp.mp3'
+    },
+    {
+        name: 'Dont Say Goodbye',
+        artist: 'Rick Astley',
+        src: 'content/Rick Astley - Dont Say Goodbye.mp3'
+    },
+    {
+        name: 'Together Forever.mp3',
+        artist: 'Rick Astley',
+        src: 'content/Rick Astley - Together Forever.mp3'
+    },
+    {
+        name: 'Giant',
+        artist: 'Rick Astley',
+        src: 'content/Rick Astley - Giant.mp3'
+    }
+];
+
+
+
+for(let i = 0;i < playList.length; i++){
+    console.log(playList[i].src)
+}
+
+
+function playPause(){
+    
+}
